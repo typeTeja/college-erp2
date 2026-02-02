50d6579c27efa5aa4bc41f7524f798d9e0d9eb3d
typeteja <webmasterteja@gmail.com>
2026-02-02 23:56:27 +0530

    Standardize communication domain: consolidate to flat structure, remove duplicate subdirectories

--- apps/api/app/domains/communication/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .communication import Circular, CircularTarget, Notification, NotificationType, NotificationChannel, NotificationLog
-
-__all__ = ["Circular", "CircularTarget", "Notification", "NotificationType", "NotificationChannel", "NotificationLog"]
--- apps/api/app/domains/communication/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .email_service import email_service
-from .sms_service import sms_service
-
-__all__ = ["email_service", "sms_service"]
